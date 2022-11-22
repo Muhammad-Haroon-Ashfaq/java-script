78ef26e3c2a5c346f08cf20956dc56c027fdba58
Muhammad Haroon chaudhary <117911242+Muhammad-Haroon-Ashfaq@users.noreply.github.com>
2022-11-21 16:57:54 -0800

    Initial commit

---
+++ per.js
@@ -0,0 +1,54 @@
+/*
+var obtMarks=+prompt("Enter obtMarks")
+var TM=+prompt("Enter TM")
+var average=(obtMarks/TM)*100
+document.write(average + ' ' + "%")
+*/
+
+
+
+
+var num1;
+var num2;
+var sum;
+num1 = Number(prompt("Enter the first value"));
+num2 = Number(prompt("Enter the second value"));
+document.write(num1 + num2);
+
+
+
+
+// var num1;
+// var num2;
+// var subtract;
+// num1 =Number(prompt("Enter the first value"));
+// num2 =Number(prompt("Enter the second value"));
+// document.write(num1 - num2);
+
+
+
+/*
+num1 =Number(prompt("Enter the first value"));
+num2 =Number(prompt("Enter the second value"));
+document.write(num1 * num2);
+*/
+
+/*
+num1 =Number(prompt("Enter the first value"));
+num2 =Number(prompt("Enter the second value"));
+document.write(num1 / num2);
+*/
+
+
+
+// // function haroon() {
+// //      let a=5;
+// //      let b=10;
+// //      c=a+b;
+// //      document.write("Your answer is" + " "+  c);
+// //      alert(c);      
+// }
+
+
+
+ 
